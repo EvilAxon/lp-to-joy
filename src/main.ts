ab2551fc630e338039dbac4dbaa47b1f3086c1b0
aXon <luca.marchetti@studioevil.com>
2023-08-23 18:02:19 +0200

    Still R&D

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,47 @@
-﻿import {LibUSBException} from "usb";
+﻿import {Device, LibUSBException} from "usb";
 
 var usb = require('usb');
 
-usb.useUsbDkBackend();
-console.log("Looking for usb controller");
-const controller = usb.findByIds(0x09db, 0x2691);
-controller.open();
 
-if (controller) {
-    console.log(controller); // Legacy device
-    console.log(controller.interfaces);
+const devices = usb.getDeviceList();
+
 /*
-    controller.interfaces[0].startPoll( 0, 2, (error:LibUSBException, buffer:Buffer, len:number, cancelled:boolean) => {
-       console.log(buffer.toString()); 
-    });
+for (const device of devices) 
+{
+    tryOpenDevice(device);
+}
 */
-    //controller.open(true);
+
+const device = usb.findByIds(0x048d, 0x5702);
+device.open();
+console.log(device.interfaces);
+const iFace = device.interface(0);
+console.log(iFace.endpoints);
+const endpoint = iFace.endpoint(129); // Sostituisci con il numero dell'endpoint corretto
+
+// Dati da inviare (in formato byte)
+const dataToSend = Buffer.from([0x01, 0x02, 0x03]);
+
+// Invia i dati all'endpoint
+endpoint.transfer( 1, (error:LibUSBException, data:any) => {
+    if (error) {
+        console.error('Errore durante il trasferimento:', error);
+    } else {
+        console.log(data);
+    }
+    device.close();
+});
+
+function tryOpenDevice( device: Device ):void 
+{
+    try 
+    {
+        device.open();
+        console.log("Succesfully opened 0x%s-0x%s", device.deviceDescriptor.idVendor.toString(16), device.deviceDescriptor.idProduct.toString(16));
+        device.close();
+    }
+    catch( exc:any )
+    {
+        console.log( "could not open device: %s ", exc.message );
+    }
 }
