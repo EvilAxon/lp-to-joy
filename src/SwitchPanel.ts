ec3ceaa44e917aa57a2967ad7ba4c65f328431d3
aXon <luca.marchetti@studioevil.com>
2023-08-25 16:46:12 +0200

    First iterations on SwitchPanel
    basic pressed/released events for buttons, to be generalized

---
+++ src/SwitchPanel.ts
@@ -0,0 +1,76 @@
+﻿import EventEmitter from "events";
+
+export class ButtonDef
+{
+    public byte:number = 0;
+    public bit:number = 0;
+    
+    public constructor(byte:number, bit:number)
+    {
+        this.byte = byte;
+        this.bit = bit;
+    }
+}
+
+export class SwitchPanel extends EventEmitter
+{
+    public vendorId:number          = 0x06a3;
+    public productId:number         = 0x0d67;
+    public readPayloadSize:number   = 3;
+
+    public buttons:Record<string, ButtonDef> =
+    { // Byte 0
+        BAT: new ButtonDef(0, 1 << 0),
+        ALT: new ButtonDef(0, 1 << 1),
+        AVIONICS: new ButtonDef(0, 1 << 2),
+        FUEL: new ButtonDef(0, 1 << 3),
+        DEICE: new ButtonDef(0, 1 << 4),
+        PILOT: new ButtonDef(0, 1 << 5),
+        COWL: new ButtonDef(0, 1 << 6),
+        PANEL: new ButtonDef(0, 1 << 7),
+        // Byte 1
+        BEACON: new ButtonDef(1, 1 << 0),
+        NAV: new ButtonDef(1, 1 << 1),
+        STROBE: new ButtonDef(1, 1 << 2),
+        TAXI: new ButtonDef(1, 1 << 3),
+        LANDING: new ButtonDef(1, 1 << 4),
+        MAG_OFF: new ButtonDef(1, 1 << 5),
+        MAG_R: new ButtonDef(1, 1 << 6),
+        MAG_L: new ButtonDef(1, 1 << 7),
+        // Byte 2
+        MAG_BOTH: new ButtonDef(2, 1 << 0),
+        MAG_START: new ButtonDef(2, 1 << 1),
+        GEAR_UP: new ButtonDef(2, 1 << 2),
+        GEAR_DOWN: new ButtonDef(2, 1 << 3),
+    }
+    
+    // Stores current buttons status
+    private currentStatus:Uint8Array = new Uint8Array([0,0,0]);
+
+    public constructor() 
+    {
+        super();
+    }
+    
+    public setNewStatus( newStatus:Buffer ):void
+    {
+        const tmpSt:Uint8Array = new Uint8Array(newStatus);
+        Object.keys(this.buttons).forEach( (key:string) =>
+        {
+            const btdef:ButtonDef = this.buttons[key];
+            const newSt:boolean = this.checkPressed(btdef, tmpSt );
+            const oldSt:boolean = this.checkPressed(btdef, this.currentStatus );
+            if (newSt && !oldSt) this.emit('buttonPressed', key);
+            if (oldSt && !newSt) this.emit('buttonReleased', key);
+        });
+        this.currentStatus = tmpSt;
+    }
+    
+    public checkPressed(button:ButtonDef, buffer?:Uint8Array ):boolean
+    {
+        if(buffer == undefined ) 
+            buffer = this.currentStatus;
+        return (buffer[button.byte] & button.bit) >0;
+    }
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,16 @@
-﻿import {Device, LibUSBException} from "usb";
+﻿import {SwitchPanel} from "./SwitchPanel";
+import {ButtonDef} from "./SwitchPanel";
 
-var usb = require('usb');
-
-
-const devices = usb.getDeviceList();
-
-/*
-for (const device of devices) 
+const panel:SwitchPanel = new SwitchPanel();
+panel.on('buttonPressed', (button:string) =>
 {
-    tryOpenDevice(device);
-}
-*/
+    console.log("%s pressed", button);
+})
 
-const bmRequestType = 0x00; // Control transfer type: OUT, Vendor-specific request, Device-to-host
-const bRequest = 0x01; // Vendor-specific request code
-const wValue = 0x0000; // Custom value (could be used for specific purposes)
-const wIndex = 0x0000; // Custom index (could be used for specific purposes)
-const timeout = 5000; // Timeout in milliseconds
-
-
-
-const device = usb.findByIds(0x06a3, 0x0d67);
-device.open();
-console.log(device.interfaces);
-const iFace = device.interface(0);
-console.log(iFace.endpoints);
-//const endpoint = iFace.endpoint(1); // Sostituisci con il numero dell'endpoint corretto
-const endpoint = iFace.endpoints[0];
-
-iFace.claim();
-// Set up the bulk transfer
-const bufferSize = 3; // Number of bytes to read
-endpoint.on("data", (data:any) =>
+panel.on('buttonReleased', (button:string) =>
 {
-   console.log(data); 
-});
+    console.log("%s released", button);
+})
 
-endpoint.on( "end",  () =>
-{
-    console.log("poling ended");
-});
-endpoint.startPoll(3, bufferSize);
-
-device.controlTransfer(
-    bmRequestType,
-    bRequest,
-    wValue,
-    wIndex,
-    Buffer.alloc(1), // No data to send in this example
-    (error:LibUSBException, data:any) => {
-        if (error) {
-            console.error('Error during control transfer:', error);
-        } else {
-            console.log('Control message sent successfully.');
-            // Process the received data if applicable
-        }
-
-        // Close the USB device after the control transfer
-        //device.close();
-    }
-);
-
-
-function tryOpenDevice( device: Device ):void 
-{
-    try 
-    {
-        device.open();
-        console.log("Succesfully opened 0x%s-0x%s", device.deviceDescriptor.idVendor.toString(16), device.deviceDescriptor.idProduct.toString(16));
-        device.close();
-    }
-    catch( exc:any )
-    {
-        console.log( "could not open device: %s ", exc.message );
-    }
-}
+panel.setNewStatus(new Buffer([1,1,1]));
+panel.setNewStatus(new Buffer([0,0,0]));
