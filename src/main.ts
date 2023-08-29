d94354e256f109a2d261b0c68d4b16cc2cacfc83
aXon <luca.marchetti@studioevil.com>
2023-08-29 18:33:52 +0200

    Config files and VJoy Mapping (still to test)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,61 @@
-﻿import {SwitchPanel} from "./SwitchPanel";
+﻿import {ButtonDef, SwitchPanel} from "./SwitchPanel";
+const flatconfig = require("flatconfig");
+const { vJoy, vJoyDevice } = require('vjoy');
 
+var defaults = {
+    MAPPINGS: {
+        VJOY_NUMBER: 1,
+        BUTTON: {
+            BAT: 1,
+            ALT: 2,
+            AVIONICS: 3,
+            FUEL: 4,
+            DEICE: 5,
+            PILOT: 6,
+            COWL: 7,
+            PANEL: 8,
+            // Byte 1
+            BEACON: 9,
+            NAV: 10,
+            STROBE: 11,
+            TAXI: 12,
+            LANDING: 13,
+            MAG_OFF: 14,
+            MAG_R: 15,
+            MAG_L: 16,
+            // Byte 2
+            MAG_BOTH: 17,
+            MAG_START: 18,
+            GEAR_UP: 19,
+            GEAR_DOWN: 20       
+        }
+    }
+}
+    
+// read configuration
+// TO-DO: Error handling
+var config = flatconfig.load(defaults,process.cwd()+"/conf.ini");
+
+// panel setup
 const panel:SwitchPanel = new SwitchPanel();
+
+// VJOY setup
+if (!vJoy.isEnabled()) {
+    console.log("vJoy is not enabled.");
+    process.exit();
+}
+
+let device = vJoyDevice.create(config.MAPPINGS.VJOY_NUMBER);
+
 panel.on('buttonPressed', (button:string) =>
 {
+    device.buttons[config.MAPPINGS.BUTTON[button]].set(true);
     console.log("%s pressed", button);
 })
 
 panel.on('buttonReleased', (button:string) =>
 {
+    device.buttons[config.MAPPINGS.BUTTON[button]].set(false);
     console.log("%s released", button);
 })
 
